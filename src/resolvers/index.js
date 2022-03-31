296eac2a463d3aef9ae4b53c8c6ab2816c30cfa8
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-03-31 17:18:52 -0300

    feat: add the input parameter to the getABook resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -4,10 +4,21 @@ const books = require('../data_source')
 const resolvers = {
     Query: {
         getAllBooks: () => books,
-        getABook: (_, {title}) => {
+        getABookByTitle: (_, {title}) => {
             return books.filter(book => book.title === title)
+        },
+        getABook: (_, {input}) => {
+            return books.filter(book => {
+                const bookObjectValues = Object.values(book)
+                const inputValue = Object.values(input)
+
+                if (bookObjectValues.includes(inputValue[0])) {
+                    return book
+                }
+            })
         }
     }
+
 }
 
 module.exports = resolvers
