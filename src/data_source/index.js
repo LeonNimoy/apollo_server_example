9d49ee260ff8203f93e7a4abba68bf1aebc47400
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-03-31 11:45:02 -0300

    chore: add publication_year data

--- src/data_source/index.js
+++ src/data_source/index.js
@@ -2,10 +2,12 @@ const books = [
     {
         title: 'The Hobbit',
         author: 'J.R.R. Tolkien',
+        publication_year: 1937
     },
     {
         title: 'The Lord of The Rings',
         author: 'J.R.R. Tolkien',
+        publication_year: 1954
     },
 ];
 
