09e38bb81fb529e7cf1887fff9a38b7fa6ed87c7
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-03-31 17:17:36 -0300

    feat: create input BookInput - add the input type to getABook query schema

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -8,9 +8,16 @@ const typeDefs = gql`
         publication_year:Int
     }
 
+    input BookInput{
+        title: String
+        author: String
+        publication_year:Int
+    }
+
     type Query {
         getAllBooks: [Book]
-        getABook(title:String!): [Book]
+        getABookByTitle(title:String): [Book]
+        getABook(input:BookInput): [Book]
     }
 `
 
