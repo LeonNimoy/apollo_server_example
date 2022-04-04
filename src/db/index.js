bec49ff9508399aaea8c6a1fa0d5cd32c69e59b1
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-04 17:00:10 -0300

    feat: update the book list

--- src/db/index.js
+++ src/db/index.js
@@ -3,18 +3,21 @@ const books = [
         id: '1',
         title: 'The Hobbit',
         author: 'J.R.R. Tolkien',
-        publication_year: 1937
+        publication_year: 1937,
+        type: 'HARDCOVER'
     },
     {
         id: '2',
         title: 'The Lord of The Rings',
         author: 'J.R.R. Tolkien',
-        publication_year: 1954
+        publication_year: 1954,
+        type: 'PAPERBACK'
     }, {
         id: '3',
         title: 'The Hitchhiker\'s Guide to the Galaxy',
         author: 'Douglas Adams',
-        publication_year: 2022
+        publication_year: 2022,
+        type: 'EBOOK'
     },
 ];
 
