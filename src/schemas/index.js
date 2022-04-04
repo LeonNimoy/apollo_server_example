e7a5d2eb44f283477ef5f15e30d38fe50ac8f968
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-04 11:50:01 -0300

    feat: update Books schema

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,21 +3,27 @@ const {gql} = require('apollo-server')
 
 const typeDefs = gql`
     type Book {
+        id: ID
         title: String
         author: String
-        publication_year:Int
+        publication_year: Int
     }
 
     input BookInput{
+        id: ID
         title: String
         author: String
-        publication_year:Int
+        publication_year: Int
     }
 
     type Query {
         getAllBooks: [Book]
         getABookByTitle(title:String): [Book]
         getABook(input:BookInput): [Book]
+    }
+
+    type Mutation {
+        updateBook(id:ID!, input: BookInput): Book!
     }
 `
 
