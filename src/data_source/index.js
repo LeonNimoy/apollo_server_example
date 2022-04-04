14545dcdf939d2b9a74d151f8e72f3e89974b00a
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-04 17:00:44 -0300

    feat: add createABook to the data source

--- src/data_source/index.js
+++ src/data_source/index.js
@@ -21,12 +21,19 @@ class BooksDataSource {
         })
     }
 
-    updateBook(id, input) {
+    updateABook(id, input) {
         const book = books.find(book => book.id === id)
         const inputKeys = Object.keys(input)
         const inputValues = Object.values(input)
         book[`${inputKeys[0]}`] = inputValues[0]
         return book
+    }
+
+    createABook(input) {
+        books.push(input)
+        const bookAdded = books[books.length - 1]
+        bookAdded['id'] = books.length
+        return bookAdded
     }
 }
 
