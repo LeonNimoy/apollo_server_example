9f5c5e7ed0b7d665b8b3f603f589284a18f2a55b
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-05 11:21:45 -0300

    feat: rename books for texts in the data source

--- src/data_source/index.js
+++ src/data_source/index.js
@@ -1,38 +1,36 @@
-const books = require("../db");
+const texts = require("../db");
 
 class BooksDataSource {
-    getAllBooks() {
-        return books
+    getAllTexts() {
+        return texts
     }
 
-    getABookByTitle(title) {
-        return books.filter(book => book.title === title)
+    getATextByTitle(title) {
+        return texts.filter(book => book.title === title)
     }
 
-    getABook(input) {
-        return books.filter(book => {
+    getAText(input) {
+        return texts.filter(book => {
             const bookObjectValues = Object.values(book)
             const inputValue = Object.values(input)
-
-            console.log(bookObjectValues, inputValue)
             if (bookObjectValues.includes(inputValue[0])) {
                 return book
             }
         })
     }
 
-    updateABook(id, input) {
-        const book = books.find(book => book.id === id)
+    updateAText(id, input) {
+        const book = texts.find(book => book.id === id)
         const inputKeys = Object.keys(input)
         const inputValues = Object.values(input)
         book[`${inputKeys[0]}`] = inputValues[0]
         return book
     }
 
-    createABook(input) {
-        books.push(input)
-        const bookAdded = books[books.length - 1]
-        bookAdded['id'] = books.length
+    createAText(input) {
+        texts.push(input)
+        const bookAdded = texts[texts.length - 1]
+        bookAdded['id'] = texts.length
         return bookAdded
     }
 }
