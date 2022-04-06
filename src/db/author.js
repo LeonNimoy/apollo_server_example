a52841e436cef0c062e95bb468b5b3cc0fc054b6
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-06 10:34:18 -0300

    feat: create author database

---
+++ src/db/author.js
@@ -0,0 +1,22 @@
+const authors = [
+    {
+        id: '1',
+        name: 'J.R.R. Tolkien',
+        age: 81,
+        alive: false,
+    },
+    {
+        id: '2',
+        name: 'Douglas Adams',
+        age: 49,
+        alive: false,
+    },
+    {
+        id: '3',
+        name: 'Albert Einstein',
+        age: 76,
+        alive: false,
+    },
+];
+
+module.exports = authors
