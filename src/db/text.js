c196a85c08a3b3436f9b8fabd78c732a270b6e6c
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-04-06 10:34:39 -0300

    feat: update text database

--- src/db/text.js
+++ src/db/text.js
@@ -2,28 +2,28 @@ const texts = [
     {
         id: '1',
         title: 'The Hobbit',
-        author: 'J.R.R. Tolkien',
+        author: '1',
         publication_year: 1937,
         type: 'HARDCOVER'
     },
     {
         id: '2',
         title: 'The Lord of The Rings',
-        author: 'J.R.R. Tolkien',
+        author: '1',
         publication_year: 1954,
         type: 'PAPERBACK'
     },
     {
         id: '3',
         title: 'The Hitchhiker\'s Guide to the Galaxy',
-        author: 'Douglas Adams',
+        author: '2',
         publication_year: 2022,
         type: 'EBOOK'
     },
     {
         id: '4',
         title: 'Zur Theorie der Brownschen Bewegung',
-        author: 'A. Einstein',
+        author: '3',
         publication_year: 1905,
         journal: 'Annalen der Physik'
     },
